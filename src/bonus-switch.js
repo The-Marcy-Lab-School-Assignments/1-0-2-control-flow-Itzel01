4d2a23d9ad56ccde38cc3f4ec3489a56f21f31d6
Itzel01 <itzel.areymi.rendon@gmail.com>
2024-04-01 18:19:07 -0400

    finished bonus-switch

--- src/bonus-switch.js
+++ src/bonus-switch.js
@@ -1,7 +1,33 @@
-const measureRainSwitch = () => {
+const measureRainSwitch = (inches) => {
+  switch (inches) {
+    case 0:
+      console.log('drought');
+      break;
+    case 1:
+      console.log('dry');
+      break;
+    case 2:
+    case 3:
+      console.log('average');
+      break;
+    case 4:
+    case 5:
+      console.log('rainy');
+      break;
+    default:
+      console.log('flood');
+  }
 };
 
-const rounderSwitch = () => {
+const rounderSwitch = (float,roundingSetting) => {
+  switch (roundingSetting) {
+    case 'up':
+      return Math.ceil(float);
+    case 'down':
+      return Math.floor(float);
+    default:
+      return Math.round(float);
+  }
 };
 
 module.exports = {
