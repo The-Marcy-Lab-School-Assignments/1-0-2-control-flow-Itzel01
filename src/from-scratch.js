b0d714033e0db9804809a167b2a424a4c18d864f
Itzel01 <itzel.areymi.rendon@gmail.com>
2024-04-01 16:12:30 -0400

    finished from scratch

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,16 +1,75 @@
-const measureRain = () => {
+const measureRain = (inches) => {
+  if(inches === 0){
+    console.log("drought")
+  } else if(inches < 2){
+    console.log("dry")
+  } else if(inches >= 2 && inches < 4){
+    console.log("average")
+  } else if(inches >= 4 && inches < 6){
+    console.log("rainy")
+  } else {
+    console.log("flood")
+  }
 };
 
-const happyBirthdayPet = () => {
+const happyBirthdayPet = (breed, age) => {
+  if(breed === "snake"){
+    console.log("Hiss hiss!")
+  } else if(breed === "cat" && age < 5){
+    console.log("Mew mew!")
+  } else if(breed === "cat" && age >= 5){
+    console.log("Meow meow!")
+  } else if(breed === "dog" && age < 5){
+    console.log("Arf arf!")
+  } else if(breed === "dog" && age >= 5 && age < 10){
+    console.log("Woof woof!")
+  } else if(breed === "dog" && age >= 10){
+    console.log("Boof!")
+  } else {
+    console.log("Happy birthday!")
+  }
 };
 
-const funTypes = () => {
+const funTypes = (jsType) => {
+  if(typeof jsType === "string"){
+    console.log("That's just some text.")
+  } else if(typeof jsType === "boolean"){
+    console.log("To bool, or not to bool?")
+  } else if(jsType === undefined){
+    console.log("Nothing, but I didn't set that.")
+  } else if(jsType === null){
+    console.log("Nothing, and I did set that.")
+  } else if(typeof jsType === "object" && !Array.isArray(jsType)){
+    console.log("Anybody got the key?")
+  } else if(Array.isArray(jsType)){
+    console.log("I order you to be indexed.")
+  } else if(typeof jsType === "number" && !isNaN(jsType)){
+    console.log("That's a good number.")
+  } else if(isNaN(jsType) === true){
+    console.log("Well, now you're just showing off.")
+  }
 };
 
-const rounder = () => {
+const rounder = (float, roundingSetting) => {
+  if(roundingSetting === "up"){
+    return Math.ceil(float)
+  } else if(roundingSetting === "down"){
+    return Math.floor(float);
+  } else {
+    return Math.round(float)
+  }
 };
 
-const fizzBuzzish = () => {
+const fizzBuzzish = (num) => {
+  if(num % 3 === 0 && num % 5 === 0){
+    console.log('fizzBuzz!')
+  } else if(num % 3 === 0){
+    console.log('fizz')
+  } else if(num % 5 === 0){
+    console.log('buzz')
+  } else {
+    console.log(num);
+  }
 };
 
 module.exports = {
